5f3a47f371e775507a53d5ad4f3d47ceca232b42
James Attard <info@jamesattard.com>
2019-04-09 17:05:06 +0200

    Added mocha tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,20 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
 
+
+let fetchedAccounts;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  fetchedAccounts = await web3.eth.getAccounts()
+
+  // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(fetchedAccounts)
+  });
+})
